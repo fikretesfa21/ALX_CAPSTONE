64b7486b589008e774405de128fa2d58af4f902a
fikretesfa21 <fikremariamdebela8@gmail.com>
2025-12-14 17:01:21 +0300

    added some view in the mood app

--- moodflick/mood/views.py
+++ moodflick/mood/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from .models import Mood
 
-# Create your views here.
+class MoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mood
+        fields = ['name', 'slug', 'icon']
+
+class MoodListView(ListAPIView):
+    queryset = Mood.objects.all()
+    serializer_class = MoodSerializer
+    permission_classes = [AllowAny]
